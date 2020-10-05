f8f5a58c4d04616b570b0dbe25fc502bc401faf2
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-10-05 10:48:01 +0200

    Initial commit

---
+++ src/main/java/com/tietoevry/bookorabackend/BookoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tietoevry.bookorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookoraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookoraBackendApplication.class, args);
+    }
+
+}
