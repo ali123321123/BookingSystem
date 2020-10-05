915baa43b07fc09d9cbd475f13ca8544a50e9e88
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-10-05 14:56:24 +0200

    Added EmployeeController Closes #5

--- src/main/java/com/tietoevry/bookorabackend/controllers/EmployeeController.java
+++ src/main/java/com/tietoevry/bookorabackend/controllers/EmployeeController.java
@@ -1,14 +1,49 @@
 package com.tietoevry.bookorabackend.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.tietoevry.bookorabackend.api.v1.model.EmployeeDTO;
+import com.tietoevry.bookorabackend.api.v1.model.EmployeeListDTO;
+import com.tietoevry.bookorabackend.services.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(EmployeeController.BASE_URL)
 public class EmployeeController {
     public static final String BASE_URL ="/api/v1/employees";
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeListDTO getEmployeeList(){
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeDTO getEmployeeById(@PathVariable Long id){
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO employeeDTO){
+        return employeeService.createNewEmployee(employeeDTO);
+    }
+
+    @PutMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeDTO updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO){
+        return employeeService.saveEmployeeByDTO(id, employeeDTO);
+    }
 
 
-
+    @DeleteMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployeeDTO(id);
+    }
 }
