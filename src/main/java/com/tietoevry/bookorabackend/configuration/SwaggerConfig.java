892872a7d732026f7b03d6cb0b9edeac1a68ab4e
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-10-05 15:02:14 +0200

    Added swagger config (with a bug to fix) Closes #13

---
+++ src/main/java/com/tietoevry/bookorabackend/configuration/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.tietoevry.bookorabackend.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
