f9eb690091088439bdc2cef6b00b6ad36af2161f
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-10-07 13:01:32 +0200

    Fixed typo in EmployeeDTO

--- src/main/java/com/tietoevry/bookorabackend/api/v1/model/EmployeeDTO.java
+++ src/main/java/com/tietoevry/bookorabackend/api/v1/model/EmployeeDTO.java
@@ -11,8 +11,8 @@ import lombok.NoArgsConstructor;
 public class EmployeeDTO {
 
     private long id ;
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
 
